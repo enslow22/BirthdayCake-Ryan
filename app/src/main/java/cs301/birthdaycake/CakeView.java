c6fef631dbb4beb26067a5686004cdc85430fc3a
Ryan <ryan.enslow@gmail.com>
2020-09-01 16:23:16 -0700

    added a second candle

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -122,6 +122,9 @@ public class CakeView extends SurfaceView {
         //Now a candle in the center
         drawCandle(canvas, cakeLeft + cakeWidth/2 - candleWidth/2, cakeTop);
 
+        //Draws a second candle on the cake
+        drawCandle(canvas, cakeLeft + cakeWidth/4 - candleWidth/2, cakeTop);
+
     }//onDraw
 
 }//class CakeView
